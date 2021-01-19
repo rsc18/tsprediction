490cfb2d4edd7400d921a2744e01b1c1dc762d1e
Chashi Mahiul Islam <ci20l@my.fsu.edu>
2021-01-19 18:01:45 -0500

    A simple fibonacci code

---
+++ chashi_exercises/fib.py
@@ -0,0 +1,26 @@
+
+
+fib_array = [0, 1]
+def fib(n):
+    if n <= 0:
+        return 0
+    if n == 1:
+        return fib_array[0]
+    elif n == 2: 
+        return fib_array[1]
+    else:
+        for i in range(2, n+1):
+            temp = fib_array[1]
+            fib_array[1] = fib_array[0] + fib_array[1]
+            fib_array[0] = temp
+        return fib_array[1]
+    
+
+if __name__ == "__main__":
+    while(True):
+        
+        print('Enter a numbers: ')
+        n = int(input())
+        print(fib(n))
+        
+        
