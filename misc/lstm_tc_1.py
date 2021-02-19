fdfe39f848b4dbc9da6b0037bb113a0109ef7678
rsc18 <rsc18@my.fsu.edu>
2021-02-19 16:13:21 -0500

    doc: changed sphix theme

--- misc/lstm_tc_1.py
+++ misc/lstm_tc_1.py
@@ -38,7 +38,7 @@ train_data_normalized = scaler.fit_transform(train_data .reshape(-1, 1))
 train_data_normalized = torch.FloatTensor(train_data_normalized).view(-1).to(device)
 
 
-train_window = 365
+train_window = 7
 
 def create_inout_sequences(input_data, tw):
     inout_seq = []
@@ -50,7 +50,6 @@ def create_inout_sequences(input_data, tw):
     return inout_seq
 
 train_inout_seq = create_inout_sequences(train_data_normalized, train_window)
-
 
 class LSTM(nn.Module):
     def __init__(self, input_size=1, hidden_layer_size=100, output_size=1):
@@ -73,7 +72,7 @@ model = LSTM().to(device)
 loss_function = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-epochs = 200
+epochs = 1
 
 for i in range(epochs):
     for seq, labels in train_inout_seq:
