502a73ff1528c8b95dd1437863ebfd362a07d4eb
Chashi Mahiul Islam <ci20l@my.fsu.edu>
2021-02-21 01:13:24 -0500

    fix: dataloader pylint 10/10

--- tsprediction/temp.py
+++ tsprediction/temp.py
@@ -4,19 +4,20 @@ DATALOADER
 Currently loads data from a csv file and provide a dataloader with given batch
 size
 """
-import random
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import torch
-from sklearn.preprocessing import MinMaxScaler
+import torch.nn as nn
 from torch.autograd import Variable
-
+from sklearn.preprocessing import MinMaxScaler
+import random
 
 def get_input_and_target(dataset, sequence_length):
     """
-    Takes timeseries data and sequence length as input and generates input
+    Takes timeseries data and sequence length as input and generates input 
     sequence and output sequence
+
     Parameters
     ----------
     dataset : numpy array
@@ -26,26 +27,24 @@ def get_input_and_target(dataset, sequence_length):
 
     Returns
     -------
-    tuple list
+    tuple
         A tuple of input samples and target samples
 
     """
-    input_seq = []
-    target_seq = []
+    x = []
+    y = []
     for i in range(len(dataset) - 2 * sequence_length + 1):
         _x = dataset[i : (i + sequence_length)]
-        _y = dataset[i + sequence_length : i + 2 * sequence_length]
-        input_seq.append(_x)
-        target_seq.append(_y)
-    data_all = []
-    for x_i, y_i in zip(np.array(input_seq), np.array(target_seq)):
-        data_all.append((x_i, y_i))
-    return data_all
+        _y = dataset[i + sequence_length : i + sequence_length + sequence_length]
+        x.append(_x)
+        y.append(_y)
+    return np.array(x), np.array(y)
 
 
 def plot_from_csv(filename: str, plot_title: str):
     """
     Plots the close values of stock data from a csv file
+
     Parameters
     ----------
     filename : str
@@ -67,29 +66,6 @@ def plot_from_csv(filename: str, plot_title: str):
     plt.show()
 
 
-def shuffled_input_target(dataset):
-    """
-    Returns shuffled input and target sequences
-    Parameters
-    ----------
-    dataset : numpy array
-        contains tuples of input and target samples
-
-    Returns
-    -------
-    tuple
-        A tuple of input samples and target samples
-
-    """
-    random.shuffle(dataset)
-    input_sequence = []
-    target_sequence = []
-    for x_i, y_i in dataset:
-        input_sequence.append(x_i)
-        target_sequence.append(y_i)
-    return input_sequence, target_sequence
-
-
 def dataloader_from_csv(
     filename: str,
     train_size_percentage: float = 0.7,
@@ -97,8 +73,9 @@ def dataloader_from_csv(
     sequence_length: int = 64,
 ):
     """
-    Loads the data from a csv file and returns train or test data
+    Loads the data from a csv file and returns train or test data 
     according to the splitting percentage.
+
     Parameters
     ----------
     filename : str
@@ -119,29 +96,44 @@ def dataloader_from_csv(
 
     """
     # loading the dataset from a csv file
-    dataset = ((pd.read_csv(filename)).iloc[:, 4].values).reshape(-1, 1)
+    dataset = pd.read_csv(filename)
+    dataset = dataset.iloc[:, 4].values
+    dataset = dataset.reshape(-1, 1)
     # normalizing the data
     min_max_scalar = MinMaxScaler()
     normalized_data = min_max_scalar.fit_transform(dataset)
 
     # getting input and target sequence
-    data_all = get_input_and_target(normalized_data, sequence_length)
-    random.shuffle(data_all)
-    input_sequence, target_sequence = shuffled_input_target(data_all)
+    input_sequence, target_sequence = get_input_and_target(
+        normalized_data, sequence_length
+    )
 
     # train and test data size
     train_size = int(len(input_sequence) * train_size_percentage)
+    test_size = len(input_sequence) - train_size
 
-    train_x = Variable(torch.Tensor(np.array(input_sequence[0:train_size])))
-    train_y = Variable(torch.Tensor(np.array(target_sequence[0:train_size])))
+    data_X = Variable(torch.Tensor(np.array(input_sequence)))
+    data_Y = Variable(torch.Tensor(np.array(target_sequence)))
 
-    test_x = Variable(
-        torch.Tensor(np.array(input_sequence[train_size : len(input_sequence)]))
-    )
-    test_y = Variable(
-        torch.Tensor(np.array(target_sequence[train_size : len(target_sequence)]))
-    )
+    data_all = [(x_i, y_i) for x_i, y_i in zip(data_X, data_Y)]
+
+    random.shuffle(data_all)
+
+    x_new = []
+    y_new = []
+    for x1, y1 in data_all:
+        x_new.append(x1.numpy())
+        y_new.append(y1.numpy())
+
+    train_X = Variable(torch.Tensor(np.array(x_new[0:train_size])))
+    train_Y = Variable(torch.Tensor(np.array(y_new[0:train_size])))
+
+    test_X = Variable(torch.Tensor(np.array(x_new[train_size : len(x_new)])))
+    test_Y = Variable(torch.Tensor(np.array(y_new[train_size : len(y_new)])))
 
     if train:
-        return train_x, train_y
-    return test_x, test_y
+        return train_X, train_Y
+    return test_X, test_Y
+
+
+trX, trY = dataloader_from_csv(filename="microsoft.csv", train=True, sequence_length=64)
