eeae7470aafca60f33d53699ba2b9a7dffc9476f
rsc18 <rsc18@my.fsu.edu>
2021-02-22 20:08:35 -0500

    feat: added plot_data.py

---
+++ figures/plot_data.py
@@ -0,0 +1,40 @@
+"""
+plots data
+"""
+import matplotlib.pyplot as plt
+import joblib
+
+      
+def plot_utils(test_data,predicted_data):
+    '''
+    
+
+    Parameters
+    ----------
+    test_data : TYPE
+        DESCRIPTION.
+    predicted_data : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    '''
+
+
+    sc=joblib.load('models/norm_model.mod')
+    data_predict = sc.inverse_transform(predicted_data)
+    
+    testY=test_data
+    dataY = (testY[:50]).view(-1,1)
+    dataY_plot = dataY.data.numpy()
+    data_predict = sc.inverse_transform(predicted_data)
+    dataY_plot = sc.inverse_transform(dataY_plot)
+    # plt.axvline(x=train_size, c='r', linestyle='--')
+    # print(dataY_plot)
+    # print(data_predict)
+    plt.plot(dataY_plot)
+    plt.plot(data_predict)
+    plt.suptitle('Time-Series Prediction')
+    plt.show()
