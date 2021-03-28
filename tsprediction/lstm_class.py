0cf91ec3ed6ecf8fe39ced77a3ccc11276dee8aa
Chashi Mahiul Islam <ci20l@my.fsu.edu>
2021-03-27 23:10:41 -0400

    refactor: 3 files edited for pylint

--- tsprediction/lstm_class.py
+++ tsprediction/lstm_class.py
@@ -6,12 +6,14 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 
-class LSTM(nn.Module):
-    
-    def __init__(self, num_classes, input_size, hidden_size, num_layers):
-        '''
-        
 
+class LSTM(nn.Module):
+    """
+    This is a LSTM class
+    """
+
+    def __init__(self, num_classes, input_size, hidden_size, num_layers):
+        """
         Parameters
         ----------
         num_classes : TYPE
@@ -27,27 +29,29 @@ class LSTM(nn.Module):
         -------
         None.
 
-        '''
-        super(LSTM, self).__init__()
-        
+        """
+        super().__init__()
+
         self.num_classes = num_classes
         self.num_layers = num_layers
         self.input_size = input_size
         self.hidden_size = hidden_size
         # self.seq_length = seq_length
-        
-        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
-                            num_layers=num_layers, batch_first=True)
+
+        self.lstm = nn.LSTM(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            batch_first=True,
+        )
         # print()
-        self.fc = nn.Linear(hidden_size, num_classes)
-        
+        self.f_c = nn.Linear(hidden_size, num_classes)
+
         # self.relu = nn.ReLU()
-        self.leaky_relu=nn.LeakyReLU(0.01)
+        self.leaky_relu = nn.LeakyReLU(0.01)
 
-    def forward(self, x):
-        '''
-        
-
+    def forward(self, data):
+        """
         Parameters
         ----------
         x : TYPE
@@ -58,20 +62,18 @@ class LSTM(nn.Module):
         out : TYPE
             DESCRIPTION.
 
-        '''
-        h_0 = Variable(torch.zeros(
-            self.num_layers, x.size(0), self.hidden_size))
-        
-        c_0 = Variable(torch.zeros(
-            self.num_layers, x.size(0), self.hidden_size))
+        """
+        h_0 = Variable(torch.zeros(self.num_layers, data.size(0), self.hidden_size))
+
+        c_0 = Variable(torch.zeros(self.num_layers, data.size(0), self.hidden_size))
         # Propagate input through LSTM
-        ula, (h_out, _) = self.lstm(x, (h_0, c_0))
-        
+        _, (h_out, _) = self.lstm(data, (h_0, c_0))
+
         # print(h_out.shape)
         h_out = h_out[-1].view(-1, self.hidden_size)
         # print(h_out.shape)
-        out = self.fc(h_out)
+        out = self.f_c(h_out)
         # out = self.relu(out)
-        out=self.leaky_relu(out)
-        
+        out = self.leaky_relu(out)
+
         return out
--- tsprediction/normalize_data.py
+++ tsprediction/normalize_data.py
@@ -3,13 +3,13 @@ Normalize time series data
 """
 
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import StandardScaler
+
+# from sklearn.preprocessing import StandardScaler
 import joblib
 
-def norm_data(dataset,norm_name):
-    '''
-    
 
+def norm_data(dataset, norm_name):
+    """
     Parameters
     ----------
     dataset : pandas series
@@ -20,16 +20,16 @@ def norm_data(dataset,norm_name):
     normalized_data : TYPE
         DESCRIPTION.
 
-    '''
-    if norm_name=='test_norm':
-        sc2=joblib.load('models/train_norm.mod')
+    """
+    if norm_name == "test_norm":
+        sc2 = joblib.load("models/train_norm.mod")
         return sc2.transform(dataset)
-    
-    # std_scaler=StandardScaler()    
+
+    # std_scaler=StandardScaler()
     # normalized_data=std_scaler.fit_transform(dataset)
     # joblib.dump(std_scaler, f'models/{norm_name}.mod')
-    
+
     min_max_scalar = MinMaxScaler()
     normalized_data = min_max_scalar.fit_transform(dataset)
-    joblib.dump(min_max_scalar,f'models/{norm_name}.mod') 
+    joblib.dump(min_max_scalar, f"models/{norm_name}.mod")
     return normalized_data
