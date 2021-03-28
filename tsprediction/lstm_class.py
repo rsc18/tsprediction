a1f58e047158defeb5a4cc7728fb1859caabc883
rsc18 <rsc18@my.fsu.edu>
2021-03-27 22:43:19 -0400

    feat: support for multiple layers

--- tsprediction/lstm_class.py
+++ tsprediction/lstm_class.py
@@ -38,7 +38,7 @@ class LSTM(nn.Module):
         
         self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
                             num_layers=num_layers, batch_first=True)
-        
+        # print()
         self.fc = nn.Linear(hidden_size, num_classes)
         
         # self.relu = nn.ReLU()
@@ -67,8 +67,9 @@ class LSTM(nn.Module):
         # Propagate input through LSTM
         ula, (h_out, _) = self.lstm(x, (h_0, c_0))
         
-        h_out = h_out.view(-1, self.hidden_size)
-        
+        # print(h_out.shape)
+        h_out = h_out[-1].view(-1, self.hidden_size)
+        # print(h_out.shape)
         out = self.fc(h_out)
         # out = self.relu(out)
         out=self.leaky_relu(out)
