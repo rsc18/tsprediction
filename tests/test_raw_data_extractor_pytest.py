cb4ba2d73653a05a51a5cdc71bd5301f1ea29a65
rsc18 <rsc18@my.fsu.edu>
2021-03-30 17:12:47 -0400

    poetry add dev pytest

--- tests/test_raw_data_extractor_pytest.py
+++ tests/test_raw_data_extractor_pytest.py
@@ -5,8 +5,15 @@ Created on Fri Jan 29 15:31:47 2021
 
 @author: chashi
 """
+import sys
+import os
 import pandas as pd
-import datasets.raw_data_extractor as rde
+
+CURRENT_DIR = os.getcwd()
+PARENT_DIR = "/".join(CURRENT_DIR.split("/")[:-1])
+sys.path.append(PARENT_DIR)
+
+import datasets.raw_data_extractor as rde 
 
 def test_get_intraday_dataset():
     """Tests get_intraday_dataset function
@@ -21,7 +28,7 @@ def test_get_intraday_dataset():
     symbol = "LI"
     key = "2JVYQIAGV1ABJD0O"
     interval = "60min"
-    dataset=pd.DataFrame(rde.get_intraday_dataset(symbol, interval, key))
+    dataset = rde.get_intraday_dataset(symbol, interval, key)
     assert len(dataset) > 0
     assert dataset.columns[3] == "4. close"
     assert pd.core.dtypes.common.is_datetime_or_timedelta_dtype(dataset.index)
