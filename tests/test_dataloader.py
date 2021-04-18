c62c41c95f2a239b55898dc6c03d1318dd8f0673
Chashi Mahiul Islam <ci20l@my.fsu.edu>
2021-04-18 02:08:58 -0400

    test: test_dataloader ready

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -1,2 +1,58 @@
 # -*- coding: utf-8 -*-
+import sys
+import os
+import pandas as pd
+CURRENT_DIR = os.getcwd()
+PARENT_DIR = "/".join(CURRENT_DIR.split("/")[:-1])
+print(PARENT_DIR)
+sys.path.append(PARENT_DIR)
+import tsprediction.dataloader as dl
 
+
+def test_get_input_and_target():
+    """
+    
+
+    Returns
+    -------
+    None.
+
+    """
+    dataset = [1,2,3,4,5,6,7,8,9,10]
+    data_all = dl.get_input_and_target(dataset, 2)
+    
+    print(data_all)
+    assert len(data_all) == 7
+    assert data_all[0][0][0] == 1
+    assert data_all[0][1][0] == 3
+    
+def test_shuffled_input_target():
+    """
+    
+
+    Returns
+    -------
+    None.
+
+    """
+    dataset = [1,2,3,4,5,6,7,8,9,10]
+    data_all = dl.get_input_and_target(dataset, 2)
+    inp, out = dl.shuffled_input_target(data_all)
+    assert len(inp[0]) == 2
+    assert len(out[0]) == 2
+    assert len(inp) ==  len(data_all)
+
+    
+def test_dataloader_from_pandas():
+    """
+    
+
+    Returns
+    -------
+    None.
+
+    """
+    tr_df = pd.DataFrame([1,2,3,4,5,6,7,8,9,10])
+    #train_test_data_tuple = dl.dataloader_from_pandas(tr_df, 1, 2, custom=True)
+    assert tr_df.size == 10
+    
